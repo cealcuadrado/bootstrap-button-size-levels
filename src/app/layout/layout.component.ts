1b0d1eb50f5740ad93d7aa30ff0c5481951ffdbd
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-09 18:02:41 -0400

    Definido contenido del layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  level: number;
+  size: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.level = 1;
   }
 
+  setLevelSize(): string {
+    return `level-${this.level}`;
+  }
+
+  setSize(): string {
+    switch (this.size) {
+      case 1: { return 'btn-sm'; break; }
+      case 2: { return 'btn-md'; break; }
+      case 3: { return 'btn-lg'; break; }
+      default: { return 'btn-md';}
+    }
+  }
 }
